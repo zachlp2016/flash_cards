c3c4f895213caf3a258a2581610ddb34b38328b9
Zach Leach <zachleach@techoverco.com>
2019-01-23 17:17:21 -0700

    Refactored flashcard_runner file.

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -11,20 +11,17 @@ class Runner
   @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
   @deck = Deck.new([@card_1, @card_2, @card_3])
   @round = Round.new(@deck)
-  @new_turn = @round.take_turn(answer_1)
-  @new_turn_2 = @round.take_turn(answer_2)
-  @new_turn_3 = @round.take_turn(answer_3)
+  # @new_turn = @round.take_turn(answer_1)
+  # @new_turn_2 = @round.take_turn(answer_2)
+  # @new_turn_3 = @round.take_turn(answer_3)
 
-
+system('clear')
 
   p "Welcome!  You're playing with 4 cards."
   p "--------------------------------------"
-  p "Ths is card number 1 out of 4."
-  p "Question: #{@new_turn.card.question}"
-  system('clear')
-  answer_1 = gets.chomp
-  @new_turn = @round.take_turn(answer_1)
-
+  p "The is card number 1 out of 4."
+    @new_turn = @round.take_turn(answer_1 = gets.chomp)
+    p "Question: #{@new_turn.card.question}"
   if answer_1 == @new_turn.card.answer
     p "Correct!"
     p "This is card number 2 out of 4."
@@ -33,26 +30,34 @@ class Runner
     p "Incorrect. Please answer again."
   end
 
-  answer_2 = gets.chomp
-  @new_turn_2 = @round.take_turn(answer_2)
+system('clear')
 
-  if answer_2 == @new_turn_2.card.answer
+  p "Welcome!  You're playing with 4 cards."
+  p "--------------------------------------"
+  p "The is card number 1 out of 4."
+    @new_turn = @round.take_turn(answer_1 = gets.chomp)
+    p "Question: #{@new_turn.card.question}"
+  if answer_1 == @new_turn.card.answer
     p "Correct!"
-    p "This is card number 3 out of 4."
-    p "Question: #{@new_turn_3.card.question}"
+    p "This is card number 2 out of 4."
+    p "Question: #{@new_turn_2.card.question}"
   else
-    p "Incorrect."
+    p "Incorrect. Please answer again."
   end
 
-  answer_3 = gets.chomp
-  @new_turn_3 = @round.take_turn(answer_3)
+  system('clear')
 
-  if answer_3 == @new_turn_3.card.answer
+  p "Welcome!  You're playing with 4 cards."
+  p "--------------------------------------"
+  p "The is card number 1 out of 4."
+    @new_turn = @round.take_turn(answer_1 = gets.chomp)
+    p "Question: #{@new_turn.card.question}"
+  if answer_1 == @new_turn.card.answer
     p "Correct!"
-    p "This is card number 3 out of 4."
-    p "Question: #{@new_turn_3.card.question}"
+    p "This is card number 2 out of 4."
+    p "Question: #{@new_turn_2.card.question}"
   else
-    p "Incorrect."
+    p "Incorrect. Please answer again."
   end
 
 end
